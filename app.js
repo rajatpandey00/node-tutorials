564ac2d3a6283edef37929c0a07c06c7c316491f
Rajat <rpandey@mdsol.com>
2019-11-09 12:02:34 +0530

    3rd commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 // util()
 const chalk = require('chalk')
 const yargs = require('yargs')
+const fs = require('fs')
 //console.log(chalk.bold.inverse.green('SUCCESS!'))
 yargs.version('1.1.0')
 
@@ -10,35 +11,55 @@ yargs.version('1.1.0')
 //Creating new note
 yargs.command({
     command: 'add',
-    description: 'Adding a Note!',
-    handler: function () {
-        console.log('Adding a Note!!')
+    description: 'Adding a Notes!',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: String,
+        },
+        body: {
+            describe: 'Notes Body',
+            type: String,
+        }
+    },
+    handler: function (argv) {
+        const title = argv.title
+        const fileName = `${title}.txt`
+        const body = argv.body
+        //console.log('Adding Notes with title', argv.title)
+        //console.log('Adding Notes with optional body', argv.body || '')
+        if(!fs.existsSync(fileName)){
+            fs.writeFileSync(fileName, body)
+         } else {
+            fs.appendFile(fileName, body, () => {}) // callBack is added for appending file
+        }
     }
  })
 //Removing note
  yargs.command({
     command: 'remove',
-    description: 'Removing Note!',
+    description: 'Removing Notes!',
     handler: function () {
-        console.log('Removing Note!!')
+        console.log('Removing Notes!!')
     }
  })
 
  //Reading note
  yargs.command({
     command: 'read',
-    description: 'Reading Note!',
+    description: 'Reading your Notes!',
     handler: function () {
-        console.log('Reading Note!!')
+        console.log('Reading your Notes!!')
     }
  })
 
  //Listing note
  yargs.command({
     command: 'list',
-    description: 'Listing Note!',
+    description: 'Listing your Notes!',
     handler: function () {
-        console.log('Listing Note!!')
+        console.log('Listing your Notes!!')
     }
  })
- console.log(yargs.argv)
+ yargs.parse()
